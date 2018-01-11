81a58f2f110253b3055516d35274b836af5ba3ba
Penezic Lumumba <mi15056@alas.matf.bg.ac.rs>
2018-01-11 04:58:10 +0100

    Fourth commit

--- main.c
+++ main.c
@@ -1,29 +1,40 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
 #include <time.h>
+#include <math.h>
 #include <GL/glut.h>
 
-#define duzina 0.1 //duzina za koju se mrdaju kuglice svakim redisplejem
+#define DODATAK 0.25 //duzina za koju se mrdaju kuglice svakim redisplejem
+#define POCETAK 41 //y koordinata od koje krece pojavljivanje loptica
 #define MAXR 40 //maximalna pozicija kuglice na y-osi pre crtanja nove na istu zicu
+#define TOLERANCIJA 0.1 //tolerancija kasnjenja/ranjenja pojavljivanja kuglice
+static float bkk = 10; //brzina kretanja kuglica
 
 typedef struct{
-	float* y; //dinamicki alociran niz
-	int n; //duzina niza
-} kuglice;
+	float* y; //dinamicki alociran niz kuglica na zici
+		  //(ima brNota polja jer je to maksimalni br kuglica na jednoj zici)
+	int n; //redni broj naredne kuglice za crtanje na zicu
+	int m; //redni broj poslednje uklonjene kuglice sa zice
+} zica;
 
-//tekuceVreme <= nizPesme[sledecaKuglica
+static char velicinaProzora; // o/p -> ceo ekran/deo ekrana
+static char mod; // e/m/h -> easy/meadium/hard
+static char stanjeIgre; //m/i/p -> meni/igra/pauza 
+static char rezultat; // g/t/z/p -> gubitak/traje/zavrsnica/pobeda
 
-static char velicinaProzora; //'o' za ceo ekran, 'p' inace
-static char mod; // e/m/h -> easy/meadium/hard 
-static char rezultat; // g/t/p gubitak/traje/pobeda
-static kuglice *pomeraj[5]; //za koliko se pomera svaka kuglica
-static float bkk = 500; //brzina kretanja kuglica
-static char stanjeIgre; //m za meni, i za igru, p za pauzu 
-static time_t pocetakIgre; //vreme pocetka igre
-static time_t tekuceVreme; //trenutni merac vremena proteklog od pocetka igre
+static zica pomeraj[5]; //podaci o svakoj zici
+
+//promenljive za racunanje vremenskih intervala
+static double pocetakIgre; //vreme pocetka igre
+static double pauza; //merac trajanja pauze
 static int sledecaKuglica; //redni broj kuglice cije se vreme sledece ceka
-static time_t *nizPesme; //niz vremena kada treba da se pojavi kruzic
+
+//podaci iz datoteke
+static double *nizPesme; //niz vremena kada treba da se pojavi kruzic
 static int brNota; //koliko ukupno ima kruzica za izlazak
+
 
 static void on_reshape(int sirina, int visina){
 	//namestanje viewporta
@@ -88,72 +99,83 @@ void loptica(int y, int linija){
 static void on_display(void){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// Postavljanje osvetljenja
-	GLfloat pozicija[] = { 0, -11, 4.5, 0 };
-	glLightfv(GL_LIGHT0, GL_POSITION, pozicija);
+	if(stanjeIgre == 'i'){ //igra
+		//postavljanje osvetljenja
+		GLfloat pozicija[] = { 0, -11, 4.5, 0 };
+		glLightfv(GL_LIGHT0, GL_POSITION, pozicija);
 
-	//postavljanje presijavanja
-	GLfloat presijavanje[] = { 20 };
-	glMaterialfv(GL_FRONT, GL_SHININESS, presijavanje);
+		//postavljanje osobina materijala
+		GLfloat presijavanje[] = { 20 };
+		glMaterialfv(GL_FRONT, GL_SHININESS, presijavanje);
+	
+		GLfloat em[] = { 0.1, 0.1, 0.1, 0 };
+		glMaterialfv(GL_FRONT, GL_EMISSION, em);
 
-	//postavljanje osobina materijala
-	GLfloat em[] = { 0.1, 0.1, 0.1, 0 };
-        glMaterialfv(GL_FRONT, GL_EMISSION, em);
+		GLfloat amb[] = { 1, 1, 1, 1 };
+		glMaterialfv(GL_FRONT, GL_AMBIENT, amb);
 
-	GLfloat amb[] = { 1, 1, 1, 1 };
-        glMaterialfv(GL_FRONT, GL_AMBIENT, amb);
+		GLfloat dif[] = { 0.3, 0.3, 0.3, 1 };
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, dif);
 
-	GLfloat dif[] = { 0.3, 0.3, 0.3, 1 };
-        glMaterialfv(GL_FRONT, GL_DIFFUSE, dif);
+		GLfloat spec[] = { 0.8, 0.8, 0.8, 1 };
+		glMaterialfv(GL_FRONT, GL_SPECULAR, spec);
 
-	GLfloat spec[] = { 0.8, 0.8, 0.8, 1 };
-        glMaterialfv(GL_FRONT, GL_SPECULAR, spec);
+		//postavljanje kamere
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		gluLookAt(0, -10,5,
+			  0,  2,0,
+			  0,  0,1);
 
-	//postavljanje kamere
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0, -10,5,
-		  0,  2,0,
-		  0,  0,1);
+		//crtanje zica
+		for(int x = -8; x <9; x = x+4){
+		    glBegin(GL_LINE_STRIP);
+			glVertex3f(x, -2, 0);
+			glVertex3f(x, 40, 0);
+		    glEnd();
+		}
 
-	//crtanje zica
-	for(int x = -8; x <9; x = x+4){
-	    glBegin(GL_LINE_STRIP);
-		glVertex3f(x, -2, 0);
-		glVertex3f(x, 40, 0);
-	    glEnd();
+		for(int i = 0; i < 5; i++){
+			for(int j = pomeraj[i].m; j < pomeraj[i].n; j++)
+					loptica(pomeraj[i].y[j], i+1);
+		}
+
+	} else if(stanjeIgre == 'm'){ //meni
+		//postavljanje kamere
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		gluLookAt(0,0,5,
+			  0,0,0,
+			  0,1,0);
+
+
+
+	} else if(stanjeIgre == 'p'){ //pauza
+		//stopira se vreme za pesmu
 	}
-
-//test
-	for(int i = 1; i < 6; i++)
-		loptica(41,i);
-
-
 
 	glutSwapBuffers();
 }
 
-void dodaj(int d, kuglice* kugliceJedneZice){
-	if(kugliceJedneZice->n == 0)
+void dodaj(float d, int i){
+	//ako nema kuglice na zici
+	if(pomeraj[i].n == 0 || pomeraj[i].n == pomeraj[i].m)
 		return;
 
-	for(int i = 0; i < kugliceJedneZice->n; i++){
-		kugliceJedneZice->y[i] -= d;
-		if(kugliceJedneZice->y[i] < -2)
+/*samo za probu pomeraj[i].m = pomeraj[i].n - 5 > 0 ? pomeraj[i].n - 5 : 0;*/
+	for(int j = pomeraj[i].m; j < pomeraj[i].n; j++){
+		pomeraj[i].y[j] = pomeraj[i].y[j] - d;
+		if(pomeraj[i].y[j] < -2)
 			rezultat = 'g';
 	}
 }
 
-float poslednja(kuglice* nizKuglica){
-	if(nizKuglica->n == 0)
+float poslednja(int i){
+	//ako nema nijedne neuklonjene kuglice, vraca se sigurno manji br
+	if(pomeraj[i].n == pomeraj[i].m || pomeraj[i].n == 0)
 		return MAXR - 1;
-	float max = nizKuglica->y[0];
-	for(int i = 1; i < nizKuglica->n; i++){
-		if(nizKuglica->y[i] > max)
-			max =  nizKuglica->y[i];
-	}
 
-	return max;
+	return pomeraj[i].y[pomeraj[i].n-1];
 }
 
 static void on_timer(int value)
@@ -164,30 +186,51 @@ static void on_timer(int value)
 
 	//sve kuglice se priblizavaju
 	for(int i = 0; i < 5; i++){
-		dodaj(duzina, pomeraj[i]);
+		dodaj(DODATAK, i);
 	}
+	
+	//ako ima neiscrtanih kuglica 
+	if(sledecaKuglica < brNota && rezultat == 't'){
+		double tekuceVreme;
+		struct timeval tv;
+		gettimeofday(&tv, NULL);
+		tekuceVreme = tv.tv_sec + 0.000001 * tv.tv_usec 
+			     - pocetakIgre - pauza;
 
-	//ako je vreme za novu kuglicu...
-	if(tekuceVreme <= nizPesme[sledecaKuglica]){
-		//...random izaberi jednu zicu i dodaj je
-		int zica = rand() % 5;
-		int i;
-		for(i = 0; i < 5; i++){
-			if(poslednja(pomeraj[(zica + i) % 5]) > MAXR)
-				zica= (zica+1) % 5;
-			else 
-				break;
-		}
-		//ako nijedna zica nije odgovarala, preskacemo notu
-		//ako je neka zica odgovarala podesavamo pomeraj
-		if(i != 5){
-			pomeraj[zica]->y = realloc(pomeraj[zica]->y, (pomeraj[zica]->n+1)*sizeof(float));
-			pomeraj[zica]->n++;
-		}
+		//ako je vreme za novu kuglicU
+		if(abs(tekuceVreme - nizPesme[sledecaKuglica]) < TOLERANCIJA){
+			//...random izaberi jednu zicu i dodaj je
+			int zica = rand() % 5;
+			int i;
+			//ako je poslednja loptica na toj zici previsoko biramo sledecu
+			for(i = 0; i < 5; i++){
+				if(poslednja((zica + i) % 5) < MAXR) {
+					zica = (zica+i) % 5;
+					break;
+				}
+			}
+			//ako nijedna zica nije odgovarala, preskacemo notu
+			//ako je neka zica odgovarala podesavamo pomeraj
+			if(i != 5){
+				//ako nije vec prosao planiran broj kuglica
+				if(pomeraj[zica].n < brNota){
+					pomeraj[zica].y[pomeraj[zica].n] = POCETAK;
+					pomeraj[zica].n++;
 
-		//ako ima jos kuglica za crtanje, uvecavamo brojac kuglica
-		if(sledecaKuglica != brNota)
+					//uvecavanje brojaca kuglica
+					sledecaKuglica++;
+				}
+			}
+	
+		//ako je prosao brzi voz za narednu kuglicu
+		} else if(tekuceVreme - nizPesme[sledecaKuglica] > 0.1){
 			sledecaKuglica++;
+		}
+	} else if(sledecaKuglica >= brNota){
+		//sacekaj da poslednja kuglica bude uklonjena
+		//if()
+		//rezultat = 'z';
+		//i onda kreni sa pomeranjem slike (zavrsnica deo u displeju)
 	}
 	
 	//iscrtavanje prozora
@@ -196,15 +239,15 @@ static void on_timer(int value)
 	//provera je li kraj igre:
 	//ako je igra predjena
 	if(rezultat == 'p'){
-		stanjeIgre = 'm';
 		//dodati funkciju za proglasavanje pobede
+		stanjeIgre = 'm';
 		sleep(5);
 	}
 
 	//ako je igra izgubljena
 	if(rezultat == 'g'){
-		stanjeIgre = 'm';
 		//dodati funkciju za proglasavanje poraza
+		stanjeIgre = 'm';
 		sleep(5);
 	}
 
@@ -224,6 +267,7 @@ static void on_keyboard(unsigned char key, int x, int y){
 			//iz cele igrice
 			//iz trenutne igre
 		//izborIzlaza();
+		//dealocirati memoriju iz key=13
 	} else if(key == 'o' || key == 'O'){
 		//proverava se je li vec bilo to slovo
 		if(velicinaProzora != 'o'){
@@ -245,18 +289,44 @@ static void on_keyboard(unsigned char key, int x, int y){
 			stanjeIgre = 'i';
 			//inicijalizacija pomeraja - nema nijedne loptice
 			for(int i = 0; i < 5; i++){
-				pomeraj[i] = malloc(sizeof (kuglice));
+/*dealocirati!*/		pomeraj[i].y = calloc(sizeof (float), brNota);
 				//ovo treba dealocirati pri izlasku 
 				//  iz igre i ulasku u meni
 				//u slucaju izlaska iz cele igrice 
 				//  bice automatski dealocirana memorija
-				pomeraj[i]->n = 0;
-				(*pomeraj[i]).y = NULL;
+				pomeraj[i].n = 0;
+				pomeraj[i].m = 0;
 			}
 			rezultat = 't';
-			pocetakIgre = time(NULL);
+			//ucitavanje nizPesme.txt
+			FILE* f = fopen("nizPesama.txt", "r");
+			if(f == NULL){
+				printf("problem sa datotekom nizPesme\n");
+				exit(1);
+			}
+			if(mod == 'e'){
+				fscanf(f, "%d\n", &brNota);
+/*dealocirati!*/		nizPesme = calloc(sizeof(double), brNota);
+				if(nizPesme == NULL){
+					printf("greska sa alokacijom memorije");
+					exit(1);
+				}
+				for(int i = 0; i < brNota; i++)
+					fscanf(f, "%lf\n", nizPesme + i);
+			}
+			fclose(f);
+
+			//inicijalizacija pocetnog vremena
+			struct timeval pom;
+			gettimeofday(&pom, NULL);
+			pocetakIgre = pom.tv_sec + 0.000001 * pom.tv_usec;
+			pauza = 0;
+
+			//sid za random biranje zice
 			srand(pocetakIgre);
-			glutTimerFunc(bkk, on_timer, 0);
+
+			//momentalni poziv funkcije
+			glutTimerFunc(0.1, on_timer, 0);
 		}
 	}
 
@@ -281,7 +351,6 @@ int main(int argc, char** argv){
 	glClearColor(0.1, 0.1, 0.1, 0);
 	mod = 'e'; //difoltni je najlaksi nivo
 	stanjeIgre = 'm'; //igra krece iz menija
-	sledecaKuglica = 0; //krecemo od prve kuglice za prikazivanje
 
 	//ukljucivanje potrebnih specifikacija
 	glEnable(GL_DEPTH_TEST);
